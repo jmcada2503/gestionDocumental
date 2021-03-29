8719c2eff55c37613224654f5f15cded3da2d243
unknown <juanmiscj25@gmail.com>
2021-03-29 10:55:17 -0500

    Solved ajax

--- gestionDocumental/static/js/search.js
+++ gestionDocumental/static/js/search.js
@@ -59,7 +59,7 @@ for (let j=0; j<usersJoined.length; j++) {
                 spaceBar2.style["background-color"] = "rgb(255, 115, 115)";
             }
             
-            //add elements to the documents
+            //add elements to the document
             flexDiv.appendChild(userName);
             flexDiv.appendChild(spaceBar);
             flexDiv.appendChild(email);
@@ -80,4 +80,90 @@ function selectUser() {
     document.querySelector("form.variable").target = "_blank";
     document.querySelector("form.variable").appendChild(idCode);
     document.querySelector("form.variable").submit();
+}
+
+function refreshInfo() {
+    let datos = new FormData(document.getElementById("searchUser"));
+    fetch("/refreshInfo/", {
+        method: "POST",
+        body: datos
+    })
+    .then(function(res) {
+        return res.text();
+    })
+    .then(function(data) {
+        document.getElementById("users").innerHTML = "";
+        usersJoined = data.split("<verified>");
+        for (let j=0; j<usersJoined.length; j++) {
+            if (usersJoined[j]!="") {
+                users = usersJoined[j].split("<end>");
+                for (let i=0; i<users.length; i++) {
+                    info = users[i].split("<next>");
+                
+                    //flexDiv
+                    flexDiv = document.createElement("div");
+                    flexDiv.className = "flexDiv contentUserInfo";
+                    flexDiv.onclick = selectUser;
+                    
+                    //name
+                    userName = document.createElement("p");
+                    userName.className = "userInfo";
+                    userName.style["width"] = "38%";
+                    userName.innerHTML = info[0];
+                    if (j==0) {
+                        userName.style["color"] = "rgb(255, 115, 115)";
+                    }
+                    
+                    //email
+                    email = document.createElement("p");
+                    email.className = "userInfo";
+                    email.style["width"] = "50%";
+                    email.style["text-align"] = "center";
+                    email.innerHTML = info[1];
+                    if (j==0) {
+                        email.style["color"] = "rgb(255, 115, 115)";
+                    }
+                    
+                    //idCode
+                    idCode = document.createElement("p");
+                    idCode.className = "userInfo";
+                    idCode.style["width"] = "10%";
+                    idCode.style["text-align"] = "right";
+                    idCode.innerHTML = info[2];
+                    if (j==0) {
+                        idCode.style["color"] = "rgb(255, 115, 115)";
+                    }
+                    
+                    //spaceBar
+                    spaceBar = document.createElement("div");
+                    spaceBar.className = "spaceBar";
+                    spaceBar.style["background-color"] = "black";
+                    spaceBar.style["height"] = "25px";
+                    spaceBar.style["margin"] = "10px 0px";
+                    if (j==0) {
+                        spaceBar.style["background-color"] = "rgb(255, 115, 115)";
+                    }
+                    
+                    //spaceBar2
+                    spaceBar2 = document.createElement("div");
+                    spaceBar2.className = "spaceBar";
+                    spaceBar2.style["background-color"] = "black";
+                    spaceBar2.style["height"] = "25px";
+                    spaceBar2.style["margin"] = "10px 0px";
+                    if (j==0) {
+                        spaceBar2.style["background-color"] = "rgb(255, 115, 115)";
+                    }
+                    
+                    //add elements to the document
+                    flexDiv.appendChild(userName);
+                    flexDiv.appendChild(spaceBar);
+                    flexDiv.appendChild(email);
+                    flexDiv.appendChild(spaceBar2);
+                    flexDiv.appendChild(idCode);
+                
+                    document.getElementById("users").appendChild(flexDiv);
+                }
+            }
+        }
+    });
 }
